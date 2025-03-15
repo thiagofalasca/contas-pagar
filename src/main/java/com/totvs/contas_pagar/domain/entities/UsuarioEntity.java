a77c42a22d8dc917b782dcaefdd617debc3177ad
Thiago <thiagofduarte12@gmail.com>
2025-03-14 23:26:09 -0300

    feat: add docker compose and fix user entity

--- src/main/java/com/totvs/contas_pagar/domain/entities/UsuarioEntity.java
+++ src/main/java/com/totvs/contas_pagar/domain/entities/UsuarioEntity.java
@@ -6,6 +6,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -14,7 +20,7 @@ import java.util.UUID;
 @Builder
 @Entity
 @Table(name = "usuarios")
-public class UsuarioEntity  {
+public class UsuarioEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -29,4 +35,47 @@ public class UsuarioEntity  {
     @Enumerated(EnumType.STRING)
     private TipoCargoUsuario cargo;
 
+    public UsuarioEntity(String nome, String email, String senha, TipoCargoUsuario cargo) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.cargo = cargo;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.cargo == TipoCargoUsuario.ADMIN)
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
