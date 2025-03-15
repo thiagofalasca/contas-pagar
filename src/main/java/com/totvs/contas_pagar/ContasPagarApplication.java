f64ffc7747046f1d373004b25cb62fa0d3101a02
Thiago <thiagofduarte12@gmail.com>
2025-03-14 23:15:49 -0300

    initial commit

---
+++ src/main/java/com/totvs/contas_pagar/ContasPagarApplication.java
@@ -0,0 +1,15 @@
+package com.totvs.contas_pagar;
+
+import lombok.extern.java.Log;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Log
+public class ContasPagarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContasPagarApplication.class, args);
+    }
+
+}
